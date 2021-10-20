94708c3a1dc0d4a0516975ca6473f3ef89aa17ad
shaydeecoder <shaydeecoder@gmail.com>
2021-10-20 21:02:00 +0100

    Added Field component and handled form events automatically

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,28 @@
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-export default StreamCreate;
+class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    /* return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      /> 
+    ); */
+    // return <input {...formProps.input} />;
+    return <input {...input} />;
+  }
+
+  render() {
+    return (
+      <form action="#">
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
