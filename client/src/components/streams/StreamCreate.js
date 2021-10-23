e6d31ea5ca93a0c11a1d05cf1d07d49054b0f551
shaydeecoder <shaydeecoder@gmail.com>
2021-10-23 11:27:59 +0100

    Added validation to StreamCreate form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+import { Field, formValues, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
   renderInput({ input, label }) {
@@ -45,6 +45,20 @@ class StreamCreate extends React.Component {
     );
   }
 }
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
 
 export default reduxForm({
   form: "streamCreate",
