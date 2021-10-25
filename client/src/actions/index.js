d8d2f3a97c918064433138a1158f8840eb2435d2
shaydeecoder <shaydeecoder@gmail.com>
2021-10-25 10:47:17 +0100

    Programmatically changed navigation after create stream submission

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -34,6 +35,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
 
   /* Do some programmatic navigation to
   get the user back to the root route */
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
