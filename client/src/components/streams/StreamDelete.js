b54246b40057565179606f1f143165196bcdadd1
shaydeecoder <shaydeecoder@gmail.com>
2021-10-29 19:17:09 +0100

    Fetched StreamDelete own data

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+import { connect } from "react-redux";
 import Modal from "../Modal";
 import history from "../../history";
+import { fetchStream } from "../../actions";
 
-const StreamDelete = () => {
+const StreamDelete = (props) => {
+  const { fetchStream, match } = props;
+
+  useEffect(() => {
+    fetchStream(match.params.id);
+  }, [fetchStream, match]);
+
   const actions = (
     <>
       <button className="ui button negative">Delete</button>
@@ -9,17 +18,24 @@ const StreamDelete = () => {
     </>
   );
 
+  const renderContent = () => {
+    if (!props.stream) return "Are you sure you want to delete this stream?";
+
+    return `Are you sure you want to delete the stream with title: ${props.stream.title}`;
+  };
+
   return (
-    <div>
-      StreamDelete
-      <Modal
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
-        onDismiss={() => history.push("/")}
-      />
-    </div>
+    <Modal
+      title="Delete Stream"
+      content={renderContent()}
+      actions={actions}
+      onDismiss={() => history.push("/")}
+    />
   );
 };
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamDelete);
