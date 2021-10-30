2316aef4644b307ef70731e05e22b88b5fbe0e80
shaydeecoder <shaydeecoder@gmail.com>
2021-10-30 19:48:19 +0100

    Fetched StreamShow own data and mapped it to the component

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,5 +1,28 @@
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
+
+const StreamShow = (props) => {
+  const { fetchStream, match } = props;
+
+  useEffect(() => {
+    fetchStream(match.params.id);
+  }, [fetchStream, match]);
+
+  if (!props.stream) return <div>Loading...</div>;
+
+  const { title, description } = props.stream;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
